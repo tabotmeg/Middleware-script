739e04c07d8262a2e38c298f7616ce342b2a71b2
Margrate <tabotmegane@yahoo.com>
2022-02-24 21:08:22 -0600

    newfile

---
+++ Sonarqube.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Description: install sonarqube
+#Author: Margrate
+#Date :2/24/2022
+
+
+sudo yum update -y
+sudo yum install java-11-openjdk-devel -y
+sudo yum install java-11-openjdk -y
+cd /opt
+sudo yum install wget -y
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip   
+sudo yum install unzip -y
+sudo unzip /opt/sonarqube-9.3.0.51899.zip
+sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
+cd /opt/sonarqube-9.3.0.51899/bin/linux-x86-64
+./sonar.sh start
